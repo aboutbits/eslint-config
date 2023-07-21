33df553254032aa6665c9c276ef3d0d0e1be4842
devgioele <gioele.devitti@aboutbits.it>
2023-07-21 10:54:22 +0200

    improve organization of repo

---
+++ configs/react.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "./index.js",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["react", "react-hooks"]
+}
