267ff5555300996b65dee73f04fc02ef7b4138c5
devgioele <gioele.devitti@aboutbits.it>
2023-07-22 08:03:02 +0200

    rm root option from ts config

--- ts.js
+++ ts.js
@@ -1,64 +1,63 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/strict-type-checked",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaVersion: 2020
   },
-  root: true,
   env: {
     node: true,
     browser: true,
     es6: true,
     commonjs: true
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     },
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        paths: ["./"]
+        paths: ['./']
       }
     }
   },
   rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
         ignoreRestSiblings: true,
-        varsIgnorePattern: "^[iI]gnored",
-        argsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^ignore"
+        varsIgnorePattern: '^[iI]gnored',
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^ignore'
       }
     ],
-    "@typescript-eslint/member-delimiter-style": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "import/order": [
-      "error",
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'import/order': [
+      'error',
       {
-        groups: ["builtin", "external", "unknown", "parent", "sibling", "index"]
+        groups: ['builtin', 'external', 'unknown', 'parent', 'sibling', 'index']
       }
     ],
-    "no-console": [
-      "error",
+    'no-console': [
+      'error',
       {
-        allow: ["warn", "error", "assert"]
+        allow: ['warn', 'error', 'assert']
       }
     ],
-    "@typescript-eslint/strict-boolean-expressions": [
-      "error",
+    '@typescript-eslint/strict-boolean-expressions': [
+      'error',
       {
         allowNullableBoolean: true
       }
