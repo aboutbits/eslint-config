cea09e95a5133a637c65e595d68f18f55198ce8a
Nils <87669551+nilshoeller@users.noreply.github.com>
2025-10-31 15:39:32 +0100

    replace eslint plugin tailwindcss with better-tailwindcss (#25)
    * add better-tailwind-css-plugin and modify settings and rules
    
    * update eslint-plugin-import
    
    * Update ts-react.js
    
    tweak better-tailwindcss config
    
    ---------
    
    Co-authored-by: Lukas Weiss <lukas.weiss@aboutbits.it>

--- ts-react.js
+++ ts-react.js
@@ -1,28 +1,53 @@
+const eslintPluginBetterTailwindcss = require('eslint-plugin-better-tailwindcss')
+const {
+  getDefaultCallees,
+  getDefaultVariables,
+} = require('eslint-plugin-better-tailwindcss/api/defaults')
+
 module.exports = {
   extends: [
     './ts.js',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:jsx-a11y/recommended',
-    'plugin:tailwindcss/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'better-tailwindcss'],
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
+    'better-tailwindcss': {
+      callees: [
+        ...getDefaultCallees(),
+        [
+          'classNames',
+          [
+            { match: 'strings' },
+            {
+              match: 'objectKeys',
+              pathPattern: '.*',
+            },
+            {
+              match: 'objectValues',
+              pathPattern: '.*',
+            },
+          ],
+        ],
+      ],
+      variables: [...getDefaultVariables(), '.*ClassNames?$'],
+    },
   },
   rules: {
     curly: ['error', 'all'],
     'react/jsx-curly-brace-presence': [
       'error',
-      { props: 'never', children: 'never', propElementValues: 'always' }
+      { props: 'never', children: 'never', propElementValues: 'always' },
     ],
     'react/self-closing-comp': [
       'error',
@@ -33,7 +58,24 @@ module.exports = {
     ],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
-    'tailwindcss/classnames-order': 'off',
-    'jsx-a11y/no-autofocus': 'off'
-  }
+    'jsx-a11y/no-autofocus': 'off',
+
+    // Recommended rules
+    ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+
+    // Configure rules
+    'better-tailwindcss/enforce-consistent-line-wrapping': ['off'], // This rule is not compatible with prettier
+    'better-tailwindcss/enforce-consistent-class-order': [
+      'error',
+      {
+        order: 'official',
+      },
+    ],
+
+    // Additional rules
+    'better-tailwindcss/enforce-consistent-variable-syntax': ['error'],
+    'better-tailwindcss/enforce-consistent-important-position': ['error'],
+    'better-tailwindcss/enforce-shorthand-classes': ['error'],
+    'better-tailwindcss/no-deprecated-classes': ['error'],
+  },
 }
