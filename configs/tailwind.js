a4a2901cbda21086f3129b603f45df9b34ed6ade
Lukas Weiss <lukas.weiss@aboutbits.it>
2026-02-24 15:08:49 +0100

    update tailwind config to new plugin major version

--- configs/tailwind.js
+++ configs/tailwind.js
@@ -1,66 +1,55 @@
 import { defineConfig } from 'eslint/config'
 import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
-import {
-  getDefaultCallees,
-  getDefaultVariables,
-} from 'eslint-plugin-better-tailwindcss/api/defaults'
+import { getDefaultSelectors } from 'eslint-plugin-better-tailwindcss/api/defaults'
+import { SelectorKind } from 'eslint-plugin-better-tailwindcss/types'
 
 export default defineConfig([
   {
     files: ['**/*.{js,cjs,mjs,jsx,ts,tsx}'],
-    plugins: {
-      'better-tailwindcss': eslintPluginBetterTailwindcss,
-    },
+    extends: [eslintPluginBetterTailwindcss.configs['recommended-error']],
     rules: {
-      // Recommended rules
-      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
-
       // Configure rules
       'better-tailwindcss/enforce-consistent-line-wrapping': ['off'], // This rule is not compatible with prettier
-      'better-tailwindcss/enforce-consistent-class-order': [
-        'error',
-        {
-          // Use official Tailwind CSS class order to be in line with the Prettier plugin
-          order: 'official',
-        },
-      ],
 
       // Additional rules
       'better-tailwindcss/enforce-consistent-variable-syntax': ['error'],
       'better-tailwindcss/enforce-consistent-important-position': ['error'],
       'better-tailwindcss/enforce-shorthand-classes': ['error'],
-      'better-tailwindcss/no-deprecated-classes': ['error'],
-      'better-tailwindcss/no-conflicting-classes': ['error'],
     },
     settings: {
       'better-tailwindcss': {
-        callees: [
-          ...getDefaultCallees(),
-          [
-            'classNames',
-            [
-              { match: 'strings' },
-              {
-                match: 'objectKeys',
-                pathPattern: '.*',
-              },
-              {
-                match: 'objectValues',
-                pathPattern: '.*',
-              },
+        selectors: [
+          ...getDefaultSelectors(),
+          {
+            kind: SelectorKind.Callee,
+            match: [
+              { type: 'strings' },
+              { type: 'objectValues', pathPattern: '.*' },
+              { type: 'objectKeys', pathPattern: '.*' },
             ],
-          ],
+            name: '^classNames$',
+          },
+          {
+            kind: SelectorKind.Variable,
+            match: [
+              { type: 'strings' },
+              { type: 'objectValues', pathPattern: '.*' },
+              { type: 'objectKeys', pathPattern: '.*' },
+            ],
+            name: '.*[cC]lassNames?$',
+          },
         ],
-        variables: [...getDefaultVariables(), '.*ClassNames?$'],
       },
     },
     /*
-  settings: {
-    'better-tailwindcss': {
-      // Entrypoint for Tailwind CSS 4
-      entryPoint: 'styles/index.css',
+    settings: {
+      'better-tailwindcss': {
+        // Entrypoint for Tailwind CSS 4
+        entryPoint: 'styles/index.css',
+        // Entrypoint for Tailwind CSS 3
+        tailwindConfig: 'tailwind.config.js',
+      },
     },
-  },
-  */
+    */
   },
 ])
